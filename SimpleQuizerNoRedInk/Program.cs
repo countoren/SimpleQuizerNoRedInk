2d964b65c9569629a63ca64afebc6b746b2adc75
Oren Rozen <oren.rozen@topmanage.com>
2017-07-30 16:20:10 -0500

    Basic data model was added

--- SimpleQuizerNoRedInk/Program.cs
+++ SimpleQuizerNoRedInk/Program.cs
@@ -5,10 +5,34 @@ using System.Text;
 
 namespace SimpleQuizerNoRedInk
 {
+    public class Strand
+    {
+        public string Name;
+        public IEnumerable<Standard> Standards;
+    }
+
+    public class Standard
+    {
+        public string Name;
+        public IEnumerable<Question> Questions;
+    }
+
+    public class Question
+    {
+        public float Difficulty;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var file = GetQuestions();
+        }
+
+        public static string GetQuestions()
+        {
+            return
+             System.IO.File.ReadAllText(@"questions.csv");
         }
     }
 }
